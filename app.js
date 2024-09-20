1891aa7234043f3d0202b6883d69d9d74f06cb55
WavuaW <wavuamuroka@gmail.com>
2024-09-20 11:41:18 +0300

    Finalising the changes with the dark and light mode as well as making the application responsive to mobile

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ arrows.forEach((arrow, i) => {
     const itemNumber = movieLists[i].querySelectorAll("img").length;
     let clickCounter = 0;
     arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 270);
         clickCounter++;
-        if(itemNumber - (4 + clickCounter) >= 0){
+        if(itemNumber - (4 + clickCounter) + (4 - ratio) >= 0){
             movieLists[i].style.transform = `translateX(${
                 movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
             }px)`;
@@ -31,6 +32,7 @@ ball.addEventListener("click",()=>{
     items.forEach((item) => {
         item.classList.toggle('active')
     })
+    ball.classList.toggle('active')
 });
     
 
